baf8aa44361d15e0c9878eff149c88c5896e9231
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2020-02-27 13:48:35 -0300

    initial commit with logo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# WhatAboutThisData
+# What about this data?
+
+```
+Esse site tem por objetivo visualizar melhor alguns dados sobre música, políticas, design de código e etc que são disponibilizados para qualquer desenvolvedor. 
+```
+
+## Project setup
+```
+yarn install
+```
+
+### Run project
+```
+yarn serve
+```
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <img src="./assets/logo2.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.container img {
+   max-width:1000px;
+   max-height:600px;
+   display: block;
+   margin-left: auto;
+   margin-right: auto
+}
+</style>
